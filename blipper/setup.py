fd61f8000595c7421fc0f7f11ba99cf845eb642b
Sivasurya Santhanam <sivasurya.in@gmail.com>
2024-04-30 21:45:03 +0530

    find packages added

--- blipper/setup.py
+++ blipper/setup.py
@@ -7,6 +7,7 @@ setup(
     license=None,
     description='Blipper library by Atenea AI',
     long_description=open('README.md').read(),
+    packages=find_packages(),
     install_requires=['requests'],
     url='http://github.com/epystemic/blipper-library',
     author='Atenea AI',
